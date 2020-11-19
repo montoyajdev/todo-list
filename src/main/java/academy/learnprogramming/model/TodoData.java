33b44c88f5bc9afe22f69788ba47a59b3ff3b7f6
montoyajdev <montoyajdev@gmail.com>
2020-11-19 00:11:32 -0800

    Created and populated dummy data into items_list.jsp

--- src/main/java/academy/learnprogramming/model/TodoData.java
+++ src/main/java/academy/learnprogramming/model/TodoData.java
@@ -9,60 +9,65 @@ import java.util.List;
 import java.util.ListIterator;
 
 public class TodoData {
-    //== fields ==
-    private static int idValue=1;
+
+    // == fields ==
+    private static int idValue = 1;
+
     private final List<TodoItem> items = new ArrayList<>();
 
     // == constructors ==
-    public TodoData(){
-        // add dummy data
+    public TodoData() {
+
+        // add some dummy data, using the addItem method so it sets the id field
         addItem(new TodoItem("first", "first details", LocalDate.now()));
         addItem(new TodoItem("second", "second details", LocalDate.now()));
         addItem(new TodoItem("third", "third details", LocalDate.now()));
     }
 
     // == public methods ==
-    public List<TodoItem> getItems(){
+    public List<TodoItem> getItems() {
         return Collections.unmodifiableList(items);
     }
 
-    public void addItem(@NonNull TodoItem toAdd){
+    public void addItem(@NonNull TodoItem toAdd) {
+
         toAdd.setId(idValue);
         items.add(toAdd);
         idValue++;
     }
 
-    public void removeItem (int id){
+    public void removeItem(int id) {
         ListIterator<TodoItem> itemIterator = items.listIterator();
-        while(itemIterator.hasNext()){
+
+        while(itemIterator.hasNext()) {
             TodoItem item = itemIterator.next();
 
-            if(item.getId() == id){
+            if(item.getId() == id) {
                 itemIterator.remove();
                 break;
             }
         }
     }
 
-    public TodoItem getItem(int id){
-        for(TodoItem item: items){
-            if(item.getId() == id){
+    public TodoItem getItem(int id) {
+        for(TodoItem item : items) {
+            if(item.getId() == id) {
                 return item;
             }
         }
         return null;
     }
 
-    public void updateItem(@NonNull TodoItem toUpdate){
+    public void updateItem(@NonNull TodoItem toUpdate) {
         ListIterator<TodoItem> itemIterator = items.listIterator();
-        while (itemIterator.hasNext()){
+
+        while(itemIterator.hasNext()) {
             TodoItem item = itemIterator.next();
 
-            if(item.equals(toUpdate)){
+            if(item.equals(toUpdate)) {
                 itemIterator.set(toUpdate);
                 break;
             }
         }
     }
-
 }
--- src/main/java/academy/learnprogramming/model/TodoItem.java
+++ src/main/java/academy/learnprogramming/model/TodoItem.java
@@ -8,6 +8,7 @@ import java.time.LocalDate;
 @Data
 @EqualsAndHashCode(of = "id")
 public class TodoItem {
+
     // == fields ==
     private int id;
     private String title;

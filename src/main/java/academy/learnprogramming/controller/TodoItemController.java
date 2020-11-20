30597a97dbd54dcc741c16d27f0e3ff86b2f805c
montoyajdev <montoyajdev@gmail.com>
2020-11-19 23:51:52 -0800

    Implemented TodoItem service

--- src/main/java/academy/learnprogramming/controller/TodoItemController.java
+++ src/main/java/academy/learnprogramming/controller/TodoItemController.java
@@ -1,8 +1,10 @@
 package academy.learnprogramming.controller;
 
 import academy.learnprogramming.model.TodoData;
+import academy.learnprogramming.service.TodoItemService;
 import academy.learnprogramming.util.Mappings;
 import academy.learnprogramming.util.ViewNames;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
@@ -10,10 +12,19 @@ import org.springframework.web.bind.annotation.ModelAttribute;
 @Controller
 public class TodoItemController {
 
+    // == fields ==
+    private final TodoItemService todoItemService;
+
+    // == constructors ==
+    @Autowired
+    public TodoItemController(TodoItemService todoItemService) {
+        this.todoItemService = todoItemService;
+    }
+
     // == model attributes ==
     @ModelAttribute
     public TodoData todoData() {
-        return new TodoData();
+        return todoItemService.getData();
     }
 
     // == handler methods ==
